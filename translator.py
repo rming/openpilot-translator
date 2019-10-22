d2b65f1e4596865ee0e854a306ae7b29fed0d88e
Rming <rmingwang@gmail.com>
2019-10-22 19:15:04 +0800

    变更目录

--- translator.py
+++ translator.py
@@ -2,41 +2,18 @@
 #-*- coding:utf-8 -*-
 
 import os
-import sys
 import re
 import shutil
 import tempfile
 import argparse
 import json
 
-def sedInplace(filename, pattern, repl):
-    '''
-    Perform the pure-Python equivalent of in-place `sed` substitution: e.g.,
-    `sed -i -e 's/'${pattern}'/'${repl}' "${filename}"`.
-    '''
-    # For efficiency, precompile the passed regular expression.
-    patternCompiled = re.compile(re.escape(pattern))
-
-    # For portability, NamedTemporaryFile() defaults to mode "w+b" (i.e., binary
-    # writing with updating). This is usually a good thing. In this case,
-    # however, binary writing imposes non-trivial encoding constraints trivially
-    # resolved by switching to text writing. Let's do that.
-    with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmpFile:
-        with open(filename) as srcFile:
-            for line in srcFile:
-                tmpFile.write(patternCompiled.sub(repl, line))
-
-    # Overwrite the original file with the munged temporary file in a
-    # manner preserving file attributes (e.g., permissions).
-    shutil.copystat(filename, tmpFile.name)
-    shutil.move(tmpFile.name, filename)
-
 class Translator:
     def __init__(self, lang):
         self.lang        = lang
         self.basePath    = os.path.dirname(os.path.abspath(__file__))
-        self.opJsonFile  = self.basePath + "/op_i18n/%s.json" % self.lang
-        self.apkJsonFile = self.basePath + "/apk_i18n/%s.json" % self.lang
+        self.opJsonFile  = self.basePath + "/i18n/openpilot/%s.json" % self.lang
+        self.apkJsonFile = self.basePath + "/i18n/apks/%s.json" % self.lang
         # 需要替换翻译信息的目录地址
         self.opPath   = self.basePath + "/openpilot/selfdrive/controls/lib"
         self.apkPath1 = self.basePath + "/openpilot-apks/offroad/js/components"
@@ -69,7 +46,7 @@ class Translator:
             # print find, replace
             for f in self.opFiles:
                 # print f.encode('utf-8')
-                sedInplace(f, find.encode('utf-8'), replace.encode('utf-8'))
+                self.sedInplace(f, find.encode('utf-8'), replace.encode('utf-8'))
 
         # 替换 apk 翻译
         for find, replace in self.apkJson.iteritems():
@@ -78,7 +55,7 @@ class Translator:
                 # print f.encode('utf-8')
                 # print  replace.encode('unicode-escape')
                 # print  repr(replace.encode('unicode-escape'))
-                sedInplace(f, find.encode('utf-8'), replace.encode('utf-8'))
+                self.sedInplace(f, find.encode('utf-8'), replace.encode('utf-8'))
 
     def getAllFileByPath(self, path):
         files = []
@@ -95,6 +72,29 @@ class Translator:
                 # print(child.encode('utf-8'))
                 files.append(child)
         return files
+
+    def sedInplace(self, filename, pattern, repl):
+        '''
+        Perform the pure-Python equivalent of in-place `sed` substitution: e.g.,
+        `sed -i -e 's/'${pattern}'/'${repl}' "${filename}"`.
+        '''
+        # For efficiency, precompile the passed regular expression.
+        patternCompiled = re.compile(re.escape(pattern))
+
+        # For portability, NamedTemporaryFile() defaults to mode "w+b" (i.e., binary
+        # writing with updating). This is usually a good thing. In this case,
+        # however, binary writing imposes non-trivial encoding constraints trivially
+        # resolved by switching to text writing. Let's do that.
+        with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmpFile:
+            with open(filename) as srcFile:
+                for line in srcFile:
+                    tmpFile.write(patternCompiled.sub(repl, line))
+
+        # Overwrite the original file with the munged temporary file in a
+        # manner preserving file attributes (e.g., permissions).
+        shutil.copystat(filename, tmpFile.name)
+        shutil.move(tmpFile.name, filename)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
